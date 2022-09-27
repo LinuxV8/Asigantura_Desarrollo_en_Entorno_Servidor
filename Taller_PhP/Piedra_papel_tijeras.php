b60682754b5fb8040dd421ceca0b3e3dabba71d1
Héctor Candelario <hector.vcc80@gmail.con>
2022-09-27 09:56:50 +0200

    Update Piedra_papel_tijeras.php

--- Taller_PhP/Piedra_papel_tijeras.php
+++ Taller_PhP/Piedra_papel_tijeras.php
@@ -3,8 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <title>Piedra, papel, tijera.
-    Extras. Programación web en PHP. Bartolomé Sintes Marco. www.mclibre.org</title>
+  <title>Piedra, papel, tijera</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <style>
   </style>
