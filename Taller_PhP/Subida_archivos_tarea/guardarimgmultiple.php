9815390ab3ca3496e180a5ef77ab91ccb708550c
Hector Vidal Candelario Cedeño <hector.vcc80@gmail.com>
2022-10-17 13:46:34 +0200

    Update guardarimgmultiple.php

--- Taller_PhP/Subida_archivos_tarea/guardarimgmultiple.php
+++ Taller_PhP/Subida_archivos_tarea/guardarimgmultiple.php
@@ -10,7 +10,14 @@
     // se incluyen esta tabla de  códigos de error que produce la subida de archivos en PHPP
     // Posibles errores de subida segun el manual de PHP
 
-    use Mpdf\Tag\S;
+    define('TAMANIO_MAXIMO_TOTAL', 300); // 300KB
+    define('TAMANIO_MAXIMO_IMAGEN', 200000); // 200KB
+
+
+    define('ERROR_NO_JPG_PNG', 9);
+    define('ERROR_TAMANIO_IMAGEN', 10);
+    define('ERROR_TAMANIO_TOTAL', 11);
+    define('ERROR_IMG_EXISTENTE', 12);
 
     $codigosErrorSubida = [
         UPLOAD_ERR_OK         => 'Subida correcta',  // Valor 0
@@ -21,12 +28,19 @@
         UPLOAD_ERR_NO_TMP_DIR => 'No existe un directorio temporal donde subir el archivo',
         UPLOAD_ERR_CANT_WRITE => 'No se pudo guardar el archivo en disco',  // permisos
         UPLOAD_ERR_EXTENSION  => 'Una extensión PHP evito la subida del archivo',  // extensión PHP
-        "Archivos existentes" => 'Ya existe un archivo con el mismo nombre',
+        ERROR_NO_JPG_PNG      => 'El archivo no es una imagen JPG o PNG',
+        ERROR_TAMANIO_IMAGEN  => 'El tamaño de la imagen excede el permitido',
+        ERROR_IMG_EXISTENTE => 'Ya existe un archivo con el mismo nombre',
+
     ];
+
+
+
     $mensaje = '';
     $directorioSubida = 'C:/imgusers';
     $tamanioTotal = 0;
     $errorFicheros = 0;
+
 
     // No se recibe nada, error al enviar el POST, se supera post_max_size
     if (count($_POST) == 0) {
@@ -37,11 +51,15 @@
         $mensaje = "  Error: no se reciben archivos ";
         echo $mensaje;
     } else {
-      /*  PARA VER EL CONTENIDO DE $FILES
+        /*  PARA VER EL CONTENIDO DE $FILES Y ERRORES*/
         echo "<pre>";
         var_dump($_FILES);
-        echo " ** Se reciben archivos **";*/
-                                                                /*      INTENTO DE OTRA FORMA PERO NO SE PUDO :(   
+
+        echo "<pre>";
+        var_dump($codigosErrorSubida);
+        echo " ** Se reciben archivos **";
+
+        /*      INTENTO DE OTRA FORMA PERO NO SE PUDO :(   
                                                                 echo "<br>El tamaño de FILES es ".sizeOf($_FILES);
                                                                 echo "<br>El tamaño de Archivos[] es ".sizeOf($_FILES['archivos']);
                                                                 echo "<br>El tamaño de los nombres de los archivos es ".sizeOf($_FILES['archivos']['name']);
@@ -55,7 +73,7 @@
         $numeroImagenes = sizeof($_FILES['archivos']['name']);
 
         $mensaje .= "<h3>Comprobando si cumplen las condiciones los archivos recibidos...</h3>";
-       
+
         //Comprobamos si los datos cumplen las condiciones
 
         for ($i = 0; $i < $numeroImagenes; $i++) {
@@ -67,20 +85,29 @@
             $temporalFichero =   $_FILES['archivos']['tmp_name'][$i];
             $errorFichero    =   $_FILES['archivos']['error'][$i];
 
-            if ($tamanioFichero > 200000) {
-                $mensaje .= "El tamaño del archivo $nombreFichero -[" . number_format(($tamanioFichero / 1000), 1, ',', '.') . " KB]- es superior a 200Kb <br />";
-                $errorFicheros = 1;
+            //COMPROBAR EL TIPO DE ARCHIVO
+            if ($tipoFichero != "image/jpeg" && $tipoFichero != "image/png") {
+                $mensaje .= "El tipo de archivo de $nombreFichero -[$tipoFichero]- no es válido <br />";
+                $errorFicheros = 9;
                 $mensaje .= "<br>***Se ha producido el error nº $errorFicheros: <em>" . $codigosErrorSubida[$errorFicheros] . '***</em> <br />';
-            } else if ($tamanioTotal > 300) {
+                //COMPROBAR EL TAMAÑO DE LA IMAGEN
+            } else if ($tamanioFichero > TAMANIO_MAXIMO_IMAGEN) {
+                $mensaje .= "El tamaño del archivo $nombreFichero -[" . number_format(($tamanioFichero / 1000), 1, ',', '.') . " KB]- es superior a 200Kb <br />";
+                $errorFicheros = 10;
+                $mensaje .= "<br>***Se ha producido el error nº $errorFicheros: <em>" . $codigosErrorSubida[$errorFicheros] . '***</em> <br />';
+                //COMPROBAR EL TAMAÑO TOTAL DE LAS IMAGENES
+            } else if ($tamanioTotal > TAMANIO_MAXIMO_TOTAL) {
                 $tamanioTotal = $tamanioTotal + number_format(($tamanioFichero / 1000), 0, ',', '.');
                 $mensaje .= "El tamaño de los archivos -[$tamanioTotal KB]- es superior a los 300Kb permitidos <br />";
-                $errorFicheros = 1;
+                $errorFicheros = 11;
                 $mensaje .= "<br>***Se ha producido el error nº $errorFicheros: <em>" . $codigosErrorSubida[$errorFicheros] . '***</em> <br />';
+                //COMPROBAR SI EXISTE YA ES IMAGEN EN LA CARPETA
             } else if (is_file($directorioSubida . "/" . $nombreFichero)) {
                 $mensaje .= "El archivo $nombreFichero ya existe en el servidor <br />";
-                $errorFicheros = 8;
+                $errorFicheros = 12;
                 $mensaje .= "<br>***Se ha producido el error nº $errorFicheros: <em>" . $codigosErrorSubida[$errorFicheros] . '***</em> <br />';
             } else {
+                // SI NO HAY ERRORES, SE SUBE EL ARCHIVO
                 $mensaje .= "El archivo $nombreFichero cumple las condiciones<br />";
                 if ($errorFicheros > 0) {
                     $mensaje .= "<br>***Se ha producido el error nº $errorFicheros: <em>" . $codigosErrorSubida[$errorFicheros] . '***</em> <br />';
